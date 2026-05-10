ab953178ddb92c575105dfcf6810da3e661fe9e2
limse0907000-cpu <limse0907000@gmail.com>
2026-05-10 22:40:37 +0900

    new file:   week9.4.py

---
+++ week9.4.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+temperature = [6,2.2,4.6,7.7,8,9.2,11.7,12.5,10.3,13.5,
+               15.2,5.9,2.9,9.2,8.6,4.6,7.2,7.7,8.3,9.5,
+               13,17,17.4,12.5,12.5,9.4,7.3,9.2,11.8,13.3,15]
+
+plt.figure(figsize=(5,3))
+plt.hist(temperature, bins=15, alpha=0.5)
+plt.xlabel('temperature')
+plt.ylabel('frequency')
+plt.show()
